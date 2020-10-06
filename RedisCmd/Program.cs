b7aedae0dc944d8ccb5186ac57a32e1b4cdd5bf1
draugr-muto <draugr.muto@gmail.com>
2020-10-07 02:54:30 +0900

    初回分のプロジェクトコミット

---
+++ RedisCmd/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+namespace RedisClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				IDatabase cache = Connection.GetDatabase();
+				var key = args[0];
+				string value = cache.StringGet(key);
+				Console.WriteLine($"Value => {value}");
+				var ttl = cache.KeyTimeToLive(key);
+				Console.WriteLine($"ttl => {ttl.Value.TotalSeconds}");
+			}
+			finally
+			{
+				lazyConnection.Value.Dispose();
+			}
+		}
+
+		private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+		{
+			string path = Directory.GetParent(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))).ToString();
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(path)
+				.AddJsonFile("appsettings.json");
+			var config = builder.Build();
+			string cacheConnetction = config["ConnectionStrings:RedisConnection"];
+			return ConnectionMultiplexer.Connect(cacheConnetction);
+		});
+
+		public static ConnectionMultiplexer Connection
+		{
+			get
+			{
+				return lazyConnection.Value;
+			}
+		}
+	}
+}
